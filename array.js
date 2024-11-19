4ff7a2ff95ef1a440aa6e501efee9b5571ab4b02
mathiashc99 <mathiasHC@live.dk>
2024-11-19 12:58:24 +0100

    init commit array

---
+++ array.js
@@ -0,0 +1,26 @@
+const favoriteFruits = ['apple','banana','orange', 'dragonfruit', 'melon'];
+
+function addfruit(fruistArray, newFruits) {
+    fruistArray.push(newFruits);
+    return fruistArray;
+}
+
+const updatedFruitArray = addfruit(favoriteFruits, "Cherry");
+console.log("Updated favorite Fruists,", updatedFruitArray);
+
+
+
+const numbers = [10,20,30,40,50];
+
+numbers.push(60);
+numbers.shift(0);
+numbers.unshift(5);
+
+let sum = 0;
+for (let number of numbers) {
+    sum += number;
+}
+console.log("sum of all numbers", sum);
+
+const greaterThenTwenty = numbers.filter((number) => number > 20);
+console.log(greaterThenTwenty);
