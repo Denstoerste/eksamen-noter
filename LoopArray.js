e1222953f03c07091d39007e153819f5acd52390
esben.dalegaard <esda0001@stud.kea.dk>
2024-11-27 14:06:31 +0100

    kom nu

---
+++ LoopArray.js
@@ -0,0 +1,18 @@
+
+
+const colors = ["red","blue","brown","yellow","pink", "babyblue","turqoise"];
+
+function displayColors(array) {
+
+    for (let i =0; i < array.length; i++){
+
+        console.log(`${i}: ${array[i]}`);
+        
+        }
+
+
+
+}
+
+displayColors(colors);
+
