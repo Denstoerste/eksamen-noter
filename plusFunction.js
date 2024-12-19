093795a57bb0de7732a63a431862bce47b2af27c
johanniemann <johan.niemann.husbjerg@gmail.com>
2024-12-19 15:29:36 +0100

    jiygiyyi

---
+++ plusFunction.js
@@ -0,0 +1,12 @@
+function addTwoNumber (number1, number2){
+    return number1 + number2
+}
+console.log(addTwoNumber(8,9))
+
+
+
+
+function multiply (number3, number4 ){
+return number3 * number4;
+}
+console.log(multiply(5, 7))
