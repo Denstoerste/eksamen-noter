cdcb36bda68e9b1ccaea2e22cea9ae0c2d5065b4
esben.dalegaard <esda0001@stud.kea.dk>
2024-12-23 12:18:16 +0100

    kom nu

--- FetchingfromApi1.js
+++ FetchingfromApi1.js
@@ -1,8 +1,22 @@
-fetch("http://dog.ceo/api/breeds/list/all")
-.then(response => response.json())
-.then(data =>{
+fetch('https://dog.ceo/api/breeds/list/all')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
 
-    console.log(data);
-    ///document.querySelector(`body`).innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;//
+        // Extract the breeds and their sub-breeds
+        const breeds = data.message;
 
-})
+        // Count the number of retriever sub-breeds
+        const retrieverSubBreeds = breeds.retriever || [];
+        console.log(`Number of retriever sub-breeds: ${retrieverSubBreeds.length}`);
+
+        // Render the first breed name to the page
+        const breedNames = Object.keys(breeds);
+        if (breedNames.length > 0) {
+            const firstBreed = breedNames[0];
+            const outputElement = document.createElement('div');
+            outputElement.textContent = `First breed: ${firstBreed}`;
+            document.body.appendChild(outputElement);
+        }
+    })
+    .catch(error => console.error('Error fetching data:', error));
