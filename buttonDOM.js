4e7128c7e9f2d1631b9d2396a376861f0d5a6d4d
esben.dalegaard <esda0001@stud.kea.dk>
2025-01-03 14:16:17 +0100

    kom nu

--- buttonDOM.js
+++ buttonDOM.js
@@ -3,3 +3,15 @@ const button = document.getElementById('submitButton');
 button.addEventListener('click', () => {
     console.log("Button clicked!");
 });
+
+
+
+document.querySelector("#voteup").addEventListener('click', () => {
+    score++;
+    document.querySelector("#score").textContent = `score: ${score}`;
+});
+
+document.querySelector("#votedown").addEventListener('click', () => {
+    score--;
+    document.querySelector("#score").textContent = `score: ${score}`;
+});
